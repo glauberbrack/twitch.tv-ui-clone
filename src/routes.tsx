ab4a9e3d681d8eb920cf185e6c225249f760e6ff
Glauber Brack <glauber@brack.com.br>
2020-08-31 18:23:02 -0300

    styling routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
@@ -12,7 +12,31 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 const Routes: React.FC = () => (
   <NavigationContainer>
-    <Navigator>
+    <Navigator
+      tabBarOptions={{
+        style: {
+          height: 90,
+          backgroundColor: colors.primary,
+          borderTopWidth: 0,
+        },
+        tabStyle: {
+          alignItems: 'center',
+          justifyContent: 'center'
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20
+        },
+        labelStyle: {
+          fontFamily: 'roboto_400',
+          fontSize: 11,
+          marginTop: 5,
+        },
+        inactiveTintColor: colors.black,
+        activeTintColor: colors.purple
+      }}
+    >
       <Screen name="Following" component={Following}
         options={{
           tabBarIcon: ({ size, focused}) => {
