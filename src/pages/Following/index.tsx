ac46cb0886c1f8ca22231b71d01bbc6616f78392
Glauber Brack <glauber@brack.com.br>
2020-09-01 00:15:00 -0300

    creating heading component

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, FlatList } from 'react-native';
 
 import { Wrapper, Container, Main } from './styles';
+
 import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+import Title from '../../components/Title';
 
 interface Item {
   key: string;
@@ -11,15 +14,71 @@ interface Item {
 }
 
 const Following: React.FC = () => {
-  React.useMemo(() => {
+  const { data, indexes } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Following</Heading>
+      },
 
+      {
+        key: 'FOLLOWED CATEGORIES',
+        render: () => <Title>Followed Categories</Title>,
+        isTitle: true
+      },
+      { key: 'C1', render: () => <View/> },
+
+      
+      {
+        key: 'LIVE CHANNELS',
+        render: () =>  <Title>Live Channels</Title>,
+        isTitle: true
+      },
+      { key: 'C2', render: () => <View/> },
+
+      
+      {
+        key: 'CONTINUE WATCHING',
+        render: () =>  <Title>Continue Watching</Title>,
+        isTitle: true
+      },
+      { key: 'C1', render: () => <View/> },
+
+      {
+        key: 'OFFLINE CHANNELS',
+        render: () =>  <Title>Offline Channels</Title>,
+        isTitle: true
+      },
+      { key: 'C4', render: () => <View/> },
+    ];
+
+    const indexes: number[] = [];
+
+    items.forEach(( item, index) => item.isTitle && indexes.push(index));
+
+    return {
+      data: items,
+      indexes
+    }
   },[])
+
   return (
     <Wrapper>
       <Container>
         <Header />
 
-        <Main />
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({ item }) => item.render()}
+            keyExtractor={item => item.key}
+            stickyHeaderIndices={indexes}
+
+            onRefresh={() => {}}
+            refreshing={true}
+
+          />
+        </Main>
       </Container>
     </Wrapper>
     
